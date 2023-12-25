f58f51e321ad54d206380d5d71c06c92421763cf
axsrza <axsrza@gmail.com>
2023-12-24 22:33:19 -0300

    Adicionar lógica para processar dados do formulário

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 
 // Adicione scripts JavaScript conforme necessário
 
-document.getElementById('customerForm').addEventListener('submit', function(event) {
+document.getElementById('customerForm').addEventListener('submit', function (event) {
     event.preventDefault(); // Evitar o envio padrão do formulário
-    console.log('Formulário enviado!');
+
+    // Obter o valor do campo de nome do cliente
+    var customerName = document.getElementById('customerName').value;
+
+    // Adicionar lógica para processar os dados, por exemplo, exibindo no console
+    console.log('Nome do Cliente:', customerName);
+
+    // Limpar o campo do nome do cliente após o envio
+    document.getElementById('customerName').value = '';
 });
