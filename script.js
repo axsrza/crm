df2b1fb486445991c429f9f7e1bfbd8e4b8ccdec
axsrza <axsrza@gmail.com>
2023-12-24 22:30:14 -0300

    Atualizar código para tratar envio do formulário

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// script.js
+
 // Adicione scripts JavaScript conforme necessário
+
+document.getElementById('customerForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar o envio padrão do formulário
+    console.log('Formulário enviado!');
+});
